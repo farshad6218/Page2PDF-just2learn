0323ef1cf432dda342ed2d6764354cb4ce4cf486
Farshad Khadempour <f.khadempour@gmail.com>
2023-09-27 12:44:42 -0400

    Initial commit

---
+++ ConvertToPDFSampleApplication.Tests/TextToPDFUnitTest.cs
@@ -0,0 +1,62 @@
+﻿
+using ConvertToPDFSampleApplication;
+using DinkToPdf;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+[TestClass]
+public class TextToPDFUnitTest
+{
+    [TestMethod]
+    public void TextToPDFbypdfShart_ShouldGeneratePDF()
+    {
+        // Arrange
+        string inputTextPath = "input.txt"; // Path to your input text file
+        string outputFileName = "output.pdf";
+        string fontName = "Arial";
+        int fontSize = 12;
+
+        var mockHttpContext = new Mock<System.Web.HttpContextBase>();
+        var mockHttpResponse = new Mock<System.Web.HttpResponseBase>();
+
+        // Set up the mock HttpContext to return the mock HttpResponse
+        mockHttpContext.Setup(ctx => ctx.Response).Returns(mockHttpResponse.Object);
+
+        // Act
+        TextToPDFClass.TextToPDFbypdfShart(inputTextPath, outputFileName, fontName, fontSize);
+
+        // Assert
+        mockHttpResponse.VerifySet(r => r.ContentType = "application/pdf");
+        mockHttpResponse.Verify(r => r.AddHeader("content-disposition", "attachment;filename=output.pdf"), Times.Once); // Verify that the output filename is set correctly
+
+    }
+
+
+    [TestMethod]
+    public void HtmlToPdfByDink_ConvertsHtmlToPdf()
+    {
+        // Arrange
+        string inputTextPath = "<html><head></head><body><div>Sample HTML File</div></body></html>"; // OR Replace with the path to your HTML input file
+        string outputFileName = "output.pdf";
+        double? topMargin = 10;
+        double? rightMargin = 10;
+        double? leftMargin = 10;
+        double? bottomMargin = 10;
+        PaperKind paperSize = PaperKind.A4;
+        Orientation pageOrientation = Orientation.Portrait;
+        int pageOffset = 0;
+
+        var mockHttpContext = new Mock<System.Web.HttpContextBase>();
+        var mockHttpResponse = new Mock<System.Web.HttpResponseBase>();
+
+        // Set up the mock HttpContext to return the mock HttpResponse
+        mockHttpContext.Setup(ctx => ctx.Response).Returns(mockHttpResponse.Object);
+
+        // Act
+        TextToPDFClass.HtmlToPdfByDink(inputTextPath, outputFileName, topMargin, rightMargin, leftMargin, bottomMargin, paperSize, pageOrientation, pageOffset);
+
+        // Assert
+        mockHttpResponse.VerifySet(r => r.ContentType = "application/pdf");
+        mockHttpResponse.Verify(r => r.AddHeader("content-disposition", "attachment;filename=output.pdf"), Times.Once); // Verify that the output filename is set correctly
+    }
+}
---
+++ ConvertToPDFSampleApplication/TextToPDF.cs
@@ -0,0 +1,112 @@
+﻿//Install-Package PdfSharp
+//Install-Package DinkToPdf
+//Install-Package DinkToPdfCopyDependencies -Version 1.0.8
+
+using System.IO;
+
+using System.Web.UI;
+
+using PdfSharp.Pdf;
+using PdfSharp.Drawing;
+using PdfSharp.Drawing.Layout;
+
+using DinkToPdf;
+using DinkToPdf.Contracts;
+
+ 
+
+namespace ConvertToPDFSampleApplication
+{
+    public class TextToPDFClass : Page
+    {
+        public static void TextToPDFbypdfShart(string inputTextPath, string outputFileName, string fontName, int fontSize)
+        {
+            string text = ReadTextFromFile(inputTextPath);
+
+            // Create a new PDF document
+            PdfDocument document = new PdfDocument();
+            PdfPage page = document.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            XFont font = new XFont(fontName, fontSize);
+            XStringFormat format = new XStringFormat();
+            format.LineAlignment = XLineAlignment.Near;
+
+            // Create a PDF text object and draw the text on the page
+            XTextFormatter tf = new XTextFormatter(gfx);
+            tf.DrawString(text, font, XBrushes.Black, new XRect(50, 50, page.Width - 100, page.Height - 100), format);
+
+            // Set response headers for PDF file
+            System.Web.HttpContext.Current.Response.ContentType = "application/pdf";
+            System.Web.HttpContext.Current.Response.AddHeader("content-disposition", "attachment;filename=" + outputFileName + ".pdf");
+
+
+            // Stream the PDF document to the response
+            using (MemoryStream stream = new MemoryStream())
+            {
+                document.Save(stream, false);
+                System.Web.HttpContext.Current.Response.BinaryWrite(stream.ToArray());
+            }
+
+            System.Web.HttpContext.Current.Response.End();
+
+        }
+
+        static string ReadTextFromFile(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+
+
+
+        //remember to use data-image convertor for alll img tags
+        //sample > https://www.cssportal.com/image-to-data/
+
+        public static void HtmlToPdfByDink(string inputTextPath, string outputFileName, double? topMargin, double? rightMargin, double? leftMargin, double? bottomMargin, PaperKind paperSize, Orientation PageOrientation, int pageOffset)
+        {
+
+            string htmlContent = ReadTextFromFile(inputTextPath);
+
+            IConverter _converter = new SynchronizedConverter(new PdfTools());
+
+            // Set response headers for PDF file
+            System.Web.HttpContext.Current.Response.ContentType = "application/pdf";
+            System.Web.HttpContext.Current.Response.AddHeader("content-disposition", "attachment;filename=" + outputFileName + ".pdf");
+
+
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                                PaperSize =  paperSize,
+                                ColorMode = ColorMode.Color,
+                                PageOffset = pageOffset,
+                                Margins = new MarginSettings() { Top = topMargin, Right = rightMargin, Left = leftMargin , Bottom = bottomMargin},
+                                Orientation = PageOrientation,
+                                                    },
+                Objects = {
+                                new ObjectSettings
+                                {
+                                    PagesCount = true,
+                                    HtmlContent = htmlContent,
+                                    WebSettings = { DefaultEncoding = "utf-8" }
+                                }
+                            }
+            };
+
+            byte[] pdfBytes = _converter.Convert(doc);
+
+
+            // Stream the PDF document to the response
+            using (MemoryStream stream = new MemoryStream())
+            {
+                System.Web.HttpContext.Current.Response.BinaryWrite(pdfBytes);
+            }
+
+            System.Web.HttpContext.Current.Response.End();
+        }
+
+    }
+}
